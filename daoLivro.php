02a3da839c9d189f4c784c67c78c085bee3fa331
unknown <JOAO_23062@lab.etec187>
2025-03-17 09:40:02 -0300

    Alteração

--- daoLivro.php
+++ daoLivro.php
@@ -2,31 +2,37 @@
 
 include_once('diaro.php');
 
-function inserir($connection, Diario $livro){
-    $data = $livro->getTit;
-    $autor = $livro->getAutor();
-    $editora = $livro->getEditora();
+function inserir($connection, Diario $diario){
+    $data = $diario->getData();
+    $nVisitantes = $diario->getVisitantes();
+    $nRetirados = $diario->getRetirados();
+    $nDevolvidos = $diario->getDevolvidos();
+    $nEmprestimo = $diario->getEmprestimo();
     $sql = "insert into diario (_data, numero_visitantes, numero_livros_retirados, numero_livros_devolvidos, numero_livros_emprestimo) values (?, ?, ?, ?);";
     $stmt = $connection->prepare($sql);
-    $stmt->bind_param("siii", $titulo, $autor, $editora);
+    $stmt->bind_param("siiii", $data, $nVisitantes, $nRetirados, $nDevolvidos, $nEmprestimo);
     $stmt->execute();
 }
 
 function lista($connection){
-    $sql = "select * from livro;";
+    $sql = "select * from diario;";
     $stmt = $connection->prepare($sql);
     $stmt->execute();
     $result = $stmt->get_result();
     while($row = $result->fetch_assoc()){
-        $titulo = $row["titulo"];
-        $autor = $row["autor"];
-        $editora = $row["editora"];
-        $id = $row["id_livro"];
+        $id = $
+        $data = $row["_data"];
+        $nVisitantes = $row["numero_visitantes"];
+        $nRetirados = $row["numero_livros_retirados"];
+        $nDevolvidos = $row["numero_livros_devolvidos"];
+        $nEmprestimo = $row["numero_livros_emprestimo"];
 
-        echo "<div class='livro'>
-                <p>$titulo</p>
-                <p>$autor</p>
-                <p>$editora</p>
+        echo "<div class='diario'>
+                <p>$data</p>
+                <p>$nVisitantes</p>
+                <p>$nRetirados</p>
+                <p>$nDevolvidos</p>
+                <p>$nEmprestimo</p>
                 <form method='post'>
                     <input type='hidden' value='$id' name='id'>
                     <input type='submit' name='deletar' value='Deletar'>
@@ -35,19 +41,21 @@ function lista($connection){
     }
 }
 
-function atualizar($connection, Livro $livro){
-    $titulo = $livro->getTitulo();
-    $autor = $livro->getAutor();
-    $editora = $livro->getEditora();
-    $id = $livro->getId();
-    $sql = "update livro set titulo = ?, autor = ?, editora = ? where id_livro = ?";
+function atualizar($connection, Diario $diario){
+    $id = $diario->getId();
+    $data = $diario->getData();
+    $nVisitantes = $diario->getVisitantes();
+    $nRetirados = $diario->getRetirados();
+    $nDevolvidos = $diario->getDevolvidos();
+    $nEmprestimo = $diario->getEmprestimo();
+    $sql = "update diario set _data = ?, numero_visitantes = ?, numero_livros_retirados = ?, numero_livros_devolvidos = ?, numero_livros_emprestimo = ? where id = ?;";
     $stmt = $connection->prepare($sql);
-    $stmt->bind_param("sssi", $titulo, $autor, $editora, $id);
+    $stmt->bind_param("siiiii", $data, $nVisitantes, $nRetirados, $nDevolvidos, $nEmprestimo, $id);
     $stmt->execute();
 }
 
 function deletar($connection, $id){
-    $sql = "delete from livro where id_livro = ?";
+    $sql = "delete from diario where id = ?";
     $stmt = $connection->prepare($sql);
     $stmt->bind_param("i", $id);
     $stmt->execute();
